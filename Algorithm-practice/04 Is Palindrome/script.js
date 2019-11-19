b61fc5f69178e087b3e9e31b530cb8d12cf25da7
Victor Ifeanyi <vifeanyi33@gmail.com>
2019-11-19 15:39:58 +0100

    Update script.js
    update script

--- Algorithm-practice/04 Is Palindrome/script.js
+++ Algorithm-practice/04 Is Palindrome/script.js
@@ -1,13 +1,8 @@
 function isPalindrome(word) {
 	var rword = ""; // container for the end result of the reversed word
-	var tempWordArr = []; // Array container to temporarily hold each letter of the supplied word during word iteration
-
-	for(var i = 0; i<word.length; i++) { // word iteration
-		tempWordArr = [...tempWordArr, word[i]]; // push each letter into tempWordArr
-	}
-
-	for(var i = 0; i<word.length; i++) {  // word iteration
-		rword += tempWordArr.pop(); // pop and add each tempWordArr letter to rword
+	
+	for(var i = word.length - 1; i >= 0; i--) {  // word iteration
+		rword += word[i];
 	}
 
 	if(word === rword) console.log(word + ' is a Palindrome'); // word is Palindrome
